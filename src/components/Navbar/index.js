f73c57cbbfcbc0c1e40843b068497965170afdcc
panumatMk <panumat.mk@gmail.com>
2022-01-30 20:13:53 +0700

    test deploy 001

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,7 +17,7 @@ const Navbar = ({toggle}) => {
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo to='/'>dolla</NavLogo>
+                    <NavLogo to='/'>dolla test deploy</NavLogo>
                     <MobileIcon onClick={toggle}>
                         <FaBars/>
                     </MobileIcon>
