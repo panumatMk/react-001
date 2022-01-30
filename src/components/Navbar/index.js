19edd380911f026ceb252e01221905aecd88b8ed
panumatMk <panumat.mk@gmail.com>
2022-01-30 19:41:39 +0700

    create react app

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {
+    MobileIcon,
+    Nav,
+    NavbarContainer,
+    NavBtn,
+    NavBtnLink,
+    NavItem,
+    NavLinks,
+    NavLogo,
+    NavMenu
+} from "./NavBarElements";
+
+const Navbar = ({toggle}) => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>dolla</NavLogo>
+                    <MobileIcon onClick={toggle}>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="signup">Sign Up</NavLinks>
+                        </NavItem>
+                        <NavBtn>
+                            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                        </NavBtn>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
